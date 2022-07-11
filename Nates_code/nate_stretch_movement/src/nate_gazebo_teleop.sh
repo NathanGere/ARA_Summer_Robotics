3c06e746ac76564240a90b7eec0356daadd4bd7d
NathanGere <Nathan_Gere@student.uml.edu>
2022-07-11 10:32:33 -0400

    added bash script to launch the gazebo teleop

---
+++ Nates_code/nate_stretch_movement/src/nate_gazebo_teleop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#use command source nate_gazebo_teleop.sh to run this file
+#roslaunch stretch_gazebo gazebo.launch needs to be run first
+
+rosrun stretch_gazebo keyboard_teleop_gazebo
