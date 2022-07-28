6c81e33ba12487269baa6638a2aa2cbece6d0af1
Matthew Flores <99828426+StixLegit14@users.noreply.github.com>
2022-07-28 12:03:05 -0400

    Update seeker_return.py

--- matt_code/depthai-python/examples/ObjectTracker/seeker_return.py
+++ matt_code/depthai-python/examples/ObjectTracker/seeker_return.py
@@ -11,12 +11,74 @@ def person_response_returner():
 
     print("From object_tracker... label responds with: " + human_detector)
 
-    # print("\n")
     if human_detector == "person":
         print("Human found!\n")
-    else:
+    elif human_detector == "background":
         print("No human found!\n")
+        print("Found background\n")
+    elif human_detector == "aeroplane":
+        print("No human found!\n")
+        print("Found aeroplane\n")
+    elif human_detector == "bicycle":
+        print("No human found!\n")
+        print("Found bicycle\n")
+    elif human_detector == "bird":
+        print("No human found!\n")
+        print("Found bird\n")
+    elif human_detector == "boat":
+        print("No human found!\n")
+        print("Found boat\n")
+    elif human_detector == "bottle":
+        print("No human found!\n")
+        print("Found bottle\n")
+    elif human_detector == "bus":
+        print("No human found!\n")
+        print("Found bus\n")
+    elif human_detector == "car":
+        print("No human found!\n")
+        print("Found car\n")
+    elif human_detector == "cat":
+        print("No human found!\n")
+        print("Found cat\n")
+    elif human_detector == "chair":
+        print("No human found!\n")
+        print("Found chair\n")
+    elif human_detector == "cow":
+        print("No human found!\n")
+        print("Found cow\n")
+    elif human_detector == "diningtable":
+        print("No human found!\n")
+        print("Found diningtable\n")
+    elif human_detector == "dog":
+        print("No human found!\n")
+        print("Found dog\n")
+    elif human_detector == "horse":
+        print("No human found!\n")
+        print("Found horse\n")
+    elif human_detector == "motorbike":
+        print("No human found!\n")
+        print("Found motorbike\n")
+    elif human_detector == "pottedplant":
+        print("No human found!\n")
+        print("Found pottedplant\n")
+    elif human_detector == "sheep":
+        print("No human found!\n")
+        print("Found sheep\n")
+    elif human_detector == "sofa":
+        print("No human found!\n")
+        print("Found sofa\n")
+    elif human_detector == "train":
+        print("No human found!\n")
+        print("Found train\n")
+    elif human_detector == "tvmoniter":
+        print("No human found!\n")
+        print("Found tvmoniter\n")
+    else:
+        print("No human found")
+        print("Found nothing :(")
     file.close()
+
+    print("Rewriting label.txt to empty to avoid miscalculations!\n")
 
     with open("label.txt", "w+") as filep:
         space = " "
